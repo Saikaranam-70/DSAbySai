7284283dc305a10f70cad8f08a9934ec93879e22
Saikaranam-70 <saikaranam46@gmail.com>
2025-05-25 16:24:40 +0530

    Initial commit

---
+++ ArrayProblems/Problem2.java
@@ -0,0 +1,32 @@
+/*
+ * Given an Array or Strings the task is to reverse the array to strings
+ * example:- 
+ *          arr = {2,11,5,10,7,8}
+ * Solution:-
+ *          arr = {8,7,10,5,11,2}
+ */
+
+
+package ArrayProblems;
+
+public class Problem2 {
+    public int[] reverseArray(int[] arr, int start, int end){
+        while(start <end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] myArray = {2,11,5,10,7,8};
+        Problem2 pr2 = new Problem2();
+        int result[] = pr2.reverseArray(myArray, 0, 5);
+        for(int i = 0; i<result.length; i++){
+            System.out.println(result[i]);
+        }
+
+    }
+}
