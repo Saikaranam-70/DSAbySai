c8874186cb89779552464a51282cfe517e7cb8b2
Saikaranam-70 <saikaranam46@gmail.com>
2025-05-25 17:36:37 +0530

    Initial commit

---
+++ ArrayProblems/Problem4.java
@@ -0,0 +1,30 @@
+/*
+ * Given an Array of integers return second maximum value.
+ * Example:-
+ *         Input:- arr={12, 34, 2,34,33,1}
+ *         Output:- 33
+ */
+
+package ArrayProblems;
+
+public class Problem4 {
+    int findSecondMax(int[] arr){
+        int max= Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]> max){
+                secondMax = max;
+                max = arr[i];
+            }else if(arr[i]> secondMax && arr[i]!=max){
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+    }
+    public static void main(String[] args) {
+        int[] myArray = {13, 34, 2,34,33,1};
+        Problem4 pr4 = new Problem4();
+        int result = pr4.findSecondMax(myArray);
+        System.out.println("Second Maximum : "+ result);
+    }
+}
