9a91b0b28a3cba221b1e802f78ab5fd5ea4b409f
Saikaranam-70 <saikaranam46@gmail.com>
2025-05-26 23:46:46 +0530

    Initial commit

---
+++ ArrayProblems/Problem7.java
@@ -0,0 +1,27 @@
+public class Problem7 {
+    int[] removeEven(int[] arr){
+        int oddCount=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] %2!=0){
+                oddCount++;
+            }
+        }
+        int[] result = new int[oddCount];
+        int idx = 0;
+        for(int i=0; i<arr.length;i++){
+            if(arr[i] % 2 !=0){
+                result[idx] = arr[i];
+                idx++;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] myArray = {3,2,4,7,10,6,5};
+        Problem1 pr1 = new Problem1();
+        int[] result = pr1.removeEven(myArray);
+        for(int i=0; i<result.length;i++){
+            System.out.println(result[i]);
+        }
+    }
+}
